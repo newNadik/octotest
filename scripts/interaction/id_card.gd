a487d1629aaadf7a090ae7b8469a7d8dd9fe693a
nadiia <i.am.nadik@gmail.com>
2026-05-04 12:43:47 +0100

    lanyard floor fix

--- scripts/interaction/id_card.gd
+++ scripts/interaction/id_card.gd
@@ -7,21 +7,32 @@ extends StaticBody3D
 @export var card_face_mesh_path: NodePath = NodePath("id_MeshInstance3D")
 @export var interactable_path: NodePath = NodePath("Interactable")
 
+
 @onready var sim = $LanyardSkeleton/SpringBoneSimulator3D
 @onready var floor = $LanyardSkeleton/SpringBoneSimulator3D/FloorCollision
 @onready var _main_scene := get_tree().current_scene
 
-const FLOOR_ACTIVE_POSITION := Vector3(0.0, 0.02, 0.0)
-const FLOOR_DISABLED_POSITION := Vector3(0.0, -1000.0, 0.0)
+const FLOOR_DISABLED_LOCAL_POSITION := Vector3(0.0, -1000.0, 0.0)
+const FLOOR_ACTIVE_HELD_POSITION := Vector3(0.0, 0.02, 0.0)
+const FLOOR_ACTIVE_DROPPED_POSITION := Vector3(0.0, -0.14, 0.0)
+@export var floor_drop_delay := 0.0
+var _interactable
+var _is_held := false
+var _drop_delay_left := 0.0
 
 
 
 func _ready() -> void:
 	var interactable = get_node_or_null(interactable_path)
+	_interactable = interactable
 	if interactable != null:
 		interactable.display_name = card_display_name
 		interactable.item_id = card_item_id
 		interactable.set_meta("access_level", access_level)
+		if interactable.has_signal("picked_up") and not interactable.picked_up.is_connected(_on_interactable_picked_up):
+			interactable.picked_up.connect(_on_interactable_picked_up)
+		if interactable.has_signal("dropped") and not interactable.dropped.is_connected(_on_interactable_dropped):
+			interactable.dropped.connect(_on_interactable_dropped)
 
 	if card_face_texture != null:
 		var face_mesh := get_node_or_null(card_face_mesh_path) as MeshInstance3D
@@ -36,7 +47,9 @@ func _ready() -> void:
 	
 	_update_floor_collision_state()
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
+	if _drop_delay_left > 0.0:
+		_drop_delay_left = maxf(0.0, _drop_delay_left - delta)
 	_update_floor_collision_state()
 
 
@@ -48,4 +61,23 @@ func _update_floor_collision_state() -> void:
 	if _main_scene != null and _main_scene.has_method("is_focus_mode_active"):
 		focus_active = bool(_main_scene.call("is_focus_mode_active"))
 
-	floor.global_position = FLOOR_DISABLED_POSITION if focus_active else FLOOR_ACTIVE_POSITION
+	if focus_active:
+		floor.global_position = FLOOR_DISABLED_LOCAL_POSITION
+	elif _is_card_held() or _drop_delay_left > 0.0:
+		floor.global_position = FLOOR_ACTIVE_HELD_POSITION
+	else:
+		floor.position = FLOOR_ACTIVE_DROPPED_POSITION
+		#print("ready card=", global_position, " floor_local=", floor.position, " floor_global=", floor.global_position)
+
+
+func _is_card_held() -> bool:
+	return _is_held
+
+
+func _on_interactable_picked_up(_interactable_ref, _actor) -> void:
+	_is_held = true
+
+
+func _on_interactable_dropped(_interactable_ref, _actor) -> void:
+	_is_held = false
+	_drop_delay_left = floor_drop_delay
